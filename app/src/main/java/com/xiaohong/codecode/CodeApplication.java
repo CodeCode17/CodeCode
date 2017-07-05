ec1d3de9f93acdb32ede780fbe36c420a6651d78
qindi <qindi1994@qq.com>
2017-07-05 19:43:04 +0800

    fix gradle

--- app/src/main/java/com/xiaohong/codecode/CodeApplication.java
+++ app/src/main/java/com/xiaohong/codecode/CodeApplication.java
@@ -2,6 +2,10 @@ package com.xiaohong.codecode;
 
 import android.app.Application;
 
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+import com.squareup.leakcanary.LeakCanary;
+
 /**
  * Created by Feng on 2017/7/5.
  */
@@ -10,6 +14,15 @@ public class CodeApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+        // Normal app init code...
 
+        //init log utils
+        Logger.addLogAdapter(new AndroidLogAdapter());
     }
 }
