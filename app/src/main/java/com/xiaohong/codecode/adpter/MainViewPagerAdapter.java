cede564c22698442cbc4384e1c00a7a4f022840f
qindi <qindi1994@qq.com>
2017-07-06 17:54:10 +0800

    移除java8支持， 侧边栏存在BUG

--- app/src/main/java/com/xiaohong/codecode/adpter/MainViewPagerAdapter.java
+++ app/src/main/java/com/xiaohong/codecode/adpter/MainViewPagerAdapter.java
@@ -3,12 +3,9 @@ package com.xiaohong.codecode.adpter;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.util.Log;
-import android.view.ViewGroup;
 
 import com.xiaohong.codecode.fragment.BaseFragment;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -17,38 +14,54 @@ import java.util.List;
  */
 
 public class MainViewPagerAdapter extends FragmentPagerAdapter {
-    private static final String TAG = "MainViewPagerAdapter";
-    List<BaseFragment> mFragments = new ArrayList<>();
-    public static final String[] fragmentTitle = new String[]{"综艺", "体育", "新闻", "热点", "头条", "军事", "历史", "科技", "人文", "地理"};
+    private List<BaseFragment> mFragments;
+    private String[] mFragmentTitle;
 
 
-    public MainViewPagerAdapter(FragmentManager fm, List<BaseFragment> fragments) {
+    public MainViewPagerAdapter(FragmentManager fm, List<BaseFragment> fragments, String[] fragmentTitle) {
         super(fm);
         this.mFragments = fragments;
+        this.mFragmentTitle = fragmentTitle;
     }
 
     @Override
     public Fragment getItem(int position) {
-//        return mFragments == null ? null : mFragments.get(position);
-        return new Fragment();
+        return mFragments.get(position);
     }
 
-    /**
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitle[position];
+    }
+    /*
+    @Override
+    public Fragment getItem(int position) {
+//        return mFragments == null ? null : mFragments.get(position);
+//        return new Fragment();
+        return mFragments.get(position);
+    }
+
+    *//**
      * instantiateItem 会调用getItem 方法， 返回一个空的fragment
      * ((BaseFragment) super.instantiateItem(container, position)); 会在父类找到对应的fragment， 进行数据的更新
      *
      * @param container
      * @param position
      * @return
-     */
-    @Override
+     *//*
+   *//* @Override
     public Object instantiateItem(ViewGroup container, int position) {
 //        BaseFragment fragment = (BaseFragment) super.instantiateItem(container, position);
         BaseFragment fragment = mFragments.get(position);
-//        fragment.setTitle(fragmentTitle.get(position));
+//        fragment.setTitle(mFragmentTitle.get(position));
 //        Log.d(TAG, "instantiateItem: ");
         return fragment;
-    }
+    }*//*
 
     @Override
     public int getCount() {
@@ -64,6 +77,6 @@ public class MainViewPagerAdapter extends FragmentPagerAdapter {
     public CharSequence getPageTitle(int position) {
 //        return mFragments.get(position).getTitle();
         Log.d(TAG, "getPageTitle: ");
-        return fragmentTitle[position];
-    }
+        return mFragmentTitle[position];
+    }*/
 }
--- app/src/main/java/com/xiaohong/codecode/mvp/mvpview/acitivity/MainActivity.java
+++ app/src/main/java/com/xiaohong/codecode/mvp/mvpview/acitivity/MainActivity.java
@@ -3,18 +3,25 @@ package com.xiaohong.codecode.mvp.mvpview.acitivity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-
 
 import com.xiaohong.codecode.R;
 import com.xiaohong.codecode.adpter.MainViewPagerAdapter;
 import com.xiaohong.codecode.fragment.BaseFragment;
 import com.xiaohong.codecode.fragment.Test0Fragment;
 import com.xiaohong.codecode.fragment.Test1Fragment;
-import com.xiaohong.codecode.mvp.mvppresenter.MainPresenter;
 import com.xiaohong.codecode.mvp.BaseActivity;
+import com.xiaohong.codecode.mvp.mvppresenter.MainPresenter;
 import com.xiaohong.codecode.mvp.mvpview.MainView;
 
 import java.util.ArrayList;
@@ -30,11 +37,20 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainVie
     ViewPager mViewPager;
     @BindView(R.id.main_tab_layout)
     TabLayout mTabLayout;
+    @BindView(R.id.main_toolbar)
+    Toolbar mMainToolbar;
+    @BindView(R.id.activity_main)
+    CoordinatorLayout mActivityMain;
+    @BindView(R.id.main_drawer_layout)
+    DrawerLayout mMainDrawerLayout;
 
 
     private List<BaseFragment> mFragments;
 
     private MainViewPagerAdapter mMainViewPagerAdapter;
+
+    private String[] mFragmentTitle;
+    private ActionBarDrawerToggle mToogle;
 
 
     @Override
@@ -47,16 +63,87 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainVie
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        initToolBar();
+        initDrawerLayout();
         initData();
+    }
 
+    private void initDrawerLayout() {
+        mMainDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
 
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                Snackbar.make(mActivityMain, "onDrawerOpened", Snackbar.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                Snackbar.make(mActivityMain, "onDrawerClosed", Snackbar.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+    }
+
+    private void initToolBar() {
+        setSupportActionBar(mMainToolbar);
+        mToogle = new ActionBarDrawerToggle(this,mMainDrawerLayout,mMainToolbar,R.string.open,R.string.close);
+        mMainDrawerLayout.addDrawerListener(mToogle);
+        mToogle.syncState();
+        mMainToolbar.setLogo(R.drawable.ic_account_circle_white_24dp);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        if (mMainDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mMainDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.toolbar_menu_search:
+                Snackbar.make(mActivityMain, item.getTitle(), Snackbar.LENGTH_SHORT).show();
+                break;
+            case R.id.toolbar_menu_download:
+                Snackbar.make(mActivityMain, item.getTitle(), Snackbar.LENGTH_SHORT).show();
+                break;
+            case R.id.toolbar_menu_games:
+                Snackbar.make(mActivityMain, item.getTitle(), Snackbar.LENGTH_SHORT).show();
+                break;
+            case R.id.toolbar_menu_tv:
+                Snackbar.make(mActivityMain, item.getTitle(), Snackbar.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void initData() {
         mFragments = new ArrayList<>();
         mFragments.add(new Test0Fragment());
         mFragments.add(new Test1Fragment());
-        mMainViewPagerAdapter = new MainViewPagerAdapter(getSupportFragmentManager(), mFragments);
+        mFragments.add(new Test0Fragment());
+        mFragments.add(new Test1Fragment());
+        mFragments.add(new Test0Fragment());
+        mFragments.add(new Test1Fragment());
+        mFragmentTitle = getResources().getStringArray(R.array.tab_layout_main_title);
+        mMainViewPagerAdapter = new MainViewPagerAdapter(getSupportFragmentManager(), mFragments, mFragmentTitle);
         mViewPager.setAdapter(mMainViewPagerAdapter);
         mTabLayout.setupWithViewPager(mViewPager);
         mTabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
